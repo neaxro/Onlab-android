d4f7750fabb2f8d5f23f8490c5cfc25b24f56f6b
neaxro <neaxro@gmail.com>
2023-03-04 11:23:04 +0100

    Kezdetleges idempotens DB script

---
+++ forrasok/db_letrehozo_script.sql
@@ -0,0 +1,110 @@
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].PeopleJobs') AND type in (N'U'))
+		drop table PeopleJobs
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].Dashboard') AND type in (N'U'))
+		drop table Dashboard
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].Shifts') AND type in (N'U'))
+		drop table Shifts
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].Positions') AND type in (N'U'))
+		drop table Positions
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].Jobs') AND type in (N'U'))
+		drop table Jobs
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].People') AND type in (N'U'))
+		drop table People
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].Wages') AND type in (N'U'))
+		drop table Wages
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].States') AND type in (N'U'))
+		drop table States
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].Roles') AND type in (N'U'))
+		drop table Roles
+
+create table [People]
+(
+	ID int identity primary key,
+	Name nvarchar(30),
+	Username nvarchar(20),
+	Email nvarchar(20),
+	Password nvarchar(30),
+	ProfilePicture text	-- Ez meg elegge kerdeses...
+)
+
+create table [Wages]
+(
+	ID int identity primary key,
+	Name nvarchar(30),
+	Wage real
+)
+
+create table [Jobs]
+(
+	ID int identity primary key,
+	Pin varchar(6),
+	Title nvarchar(20),
+	Description nvarchar(100),
+	PeopleID int references People(ID) --creator
+)
+
+create table [States]
+(
+	ID int identity primary key,
+	Description nvarchar(100)
+)
+
+create table [Shifts]
+(
+	ID int identity primary key,
+	StartTime datetime2,
+	EndTime datetime2,
+	EarnedMoney real,
+	PeopleID int references People(ID),
+	JobID int references Jobs(ID),
+	WageID int references Wages(ID),
+	StatusID int references States(ID)
+)
+
+create table [Dashboard]
+(
+	ID int identity primary key,
+	Title nvarchar(20),
+	Message text,
+	CreationTime datetime2,
+	JobID int references Jobs(ID),
+	PeopleID int references People(ID),
+	WageID int references Wages(ID)
+)
+
+create table [Roles]
+(
+	ID int identity primary key,
+	Title nvarchar(20),
+	CanMakeMessage bit,
+	CanManageShifts bit,
+	CanAddRole bit,
+	CanLoseRole bit,
+	CanChangeWage bit
+)
+
+create table [Positions]
+(
+	ID int identity primary key,
+	Time datetime2,
+	Longitude real,
+	Latitude real,
+	PeopleID int references People(ID)
+)
+
+create table [PeopleJobs]
+(
+	ID int identity primary key,
+	JobID int references Jobs(ID),
+	PeopleID int references People(ID),
+	RoleID int references Roles(ID),
+	WageID int references Wages(ID)
+)
