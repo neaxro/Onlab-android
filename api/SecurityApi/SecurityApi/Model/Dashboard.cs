87589548b5779ebf38db361a9bde1b029a9b9532
Nemes Axel Roland <neaxro@gmail.com>
2023-03-13 22:33:57 +0100

    Scaffolding kész :100:

---
+++ api/SecurityApi/SecurityApi/Model/Dashboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecurityApi.Model;
+
+public partial class Dashboard
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Message { get; set; }
+
+    public DateTime? CreationTime { get; set; }
+
+    public int? JobId { get; set; }
+
+    public int? PeopleId { get; set; }
+
+    public int? WageId { get; set; }
+
+    public virtual Job? Job { get; set; }
+
+    public virtual Person? People { get; set; }
+
+    public virtual Wage? Wage { get; set; }
+}
---
+++ api/SecurityApi/SecurityApi/Model/State.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecurityApi.Model;
+
+public partial class State
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<Shift> Shifts { get; } = new List<Shift>();
+}
