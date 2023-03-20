8b8e1dee2d9c10f9803d79032cca75162e26faa8
neaxro <neaxro@gmail.com>
2023-03-20 12:26:49 +0100

    Create ModelToDtoConverter.cs
    Egy helyen legyenek a konverterek mert a Job-nál is kell használni többet

---
+++ api/SecurityApi/SecurityApi/Converters/ModelToDtoConverter.cs
@@ -0,0 +1,37 @@
+﻿namespace SecurityApi.Converters
+{
+    public class ModelToDtoConverter
+    {
+        private static ModelToDtoConverter converter = null;
+        private static readonly object lockObject = new object();
+
+        public ModelToDtoConverter() { }
+
+        public static ModelToDtoConverter Instance
+        {
+            get
+            {
+                lock(lockObject)
+                {
+                    if (converter == null)
+                    {
+                        converter = new ModelToDtoConverter();
+                    }
+
+                    return converter;
+                }
+            }
+        }
+
+        public Dtos.Person ToModel(Model.Person person)
+        {
+            return new Dtos.Person(person.Id, person.Name, person.Username, person.Nickname, person.Email, person.ProfilePicture);
+        }
+
+        public Dtos.Job ToModel(Model.Job job)
+        {
+            Dtos.Person owner = ToModel(job.People);
+            return new Dtos.Job(job.Id, job.Title, job.Description, owner);
+        }
+    }
+}
