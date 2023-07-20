71758796986276614137022b45010caa870f2525
Nemes Axel Roland <neaxro@gmail.com>
2023-07-20 09:33:55 +0200

    Create MySearchBar.kt

---
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/common/MySearchBar.kt
@@ -0,0 +1,63 @@
+package hu.bme.aut.android.securityapp.ui.feature.common
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@Composable
+fun MySearchBar(
+    onSearch: (String) -> Unit,
+){
+    var text by remember { mutableStateOf("Axel") }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
+
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = {
+            Text(text = "Search")
+        },
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+        },
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(onSearch = {
+            onSearch(text)
+            // Hide the keyboard after submitting the search
+            keyboardController?.hide()
+            //or hide keyboard
+            focusManager.clearFocus()
+
+        })
+    )
+}
+
+@Composable
+@Preview(showBackground = false)
+fun MySearchBarPreview(){
+    MySearchBar(
+        onSearch = {}
+    )
+}
