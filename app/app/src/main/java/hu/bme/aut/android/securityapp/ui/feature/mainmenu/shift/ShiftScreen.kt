4950dfe918ad8d4ae3fd5e9c69b999d3292e36f8
Nemes Axel Roland <neaxro@gmail.com>
2023-09-11 22:07:23 +0200

    VS update

--- app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/mainmenu/shift/ShiftScreen.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/mainmenu/shift/ShiftScreen.kt
@@ -3,20 +3,35 @@ package hu.bme.aut.android.securityapp.ui.feature.mainmenu.menus
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import hu.bme.aut.android.securityapp.ui.feature.common.MyTopAppBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShiftScreen(){
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ){
-        Text(text = "SHIFT SCREEN")
+    Scaffold(
+        topBar = {
+            MyTopAppBar(title = "Shift")
+        }
+    ) {
+        val paddingTop = it.calculateTopPadding()
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = paddingTop),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Text(text = "SHIFT SCREEN")
+        }
     }
 }
 
