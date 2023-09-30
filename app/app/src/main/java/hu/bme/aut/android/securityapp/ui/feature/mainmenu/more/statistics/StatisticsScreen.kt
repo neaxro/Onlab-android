92d70f2a9142544f79111cff7cf45e4a8cbbb137
Nemes Axel Roland <neaxro@gmail.com>
2023-09-30 22:36:40 +0200

    Create the basic Statistics screen and viewmodel

--- app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/mainmenu/more/statistics/StatisticsScreen.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/mainmenu/more/statistics/StatisticsScreen.kt
@@ -2,24 +2,29 @@ package hu.bme.aut.android.securityapp.ui.feature.mainmenu.more.statistics
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import hu.bme.aut.android.securityapp.ui.feature.common.MyTopAppBar
+import hu.bme.aut.android.securityapp.ui.feature.common.SimpleShift
+import hu.bme.aut.android.securityapp.ui.feature.common.StatisticsTabScreen
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun StatisticsScreen(
     navigateBack: () -> Unit,
+    viewModel: StatisticsScreenViewModel = hiltViewModel()
 ){
     Scaffold(
         topBar = {
@@ -32,20 +37,33 @@ fun StatisticsScreen(
         }
     ) {
         val paddingTop = it.calculateTopPadding()
+        val lazyState = rememberLazyListState()
+
+        val shifts = viewModel.shifts.collectAsState().value
 
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(top = paddingTop)
         ){
-            FlowRow(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(5.dp)
+            StatisticsTabScreen(onChange = { tabIndex ->
+                when(tabIndex){
+                    1 -> viewModel.changeFilter(StatisticsFilter.Accepted)
+                    2 -> viewModel.changeFilter(StatisticsFilter.Denied)
+                    3 -> viewModel.changeFilter(StatisticsFilter.Pending)
+                    else -> viewModel.changeFilter(StatisticsFilter.All)
+                }
+            })
 
-            ) {
-                FilterChip(selected = true, onClick = { /*TODO*/ }, label = { Text(text = "Acceped") })
-                FilterChip(selected = false, onClick = { /*TODO*/ }, label = { Text(text = "Denied") })
+            LazyColumn(
+                state = lazyState
+            ){
+                items(items = shifts){ shift ->
+                    SimpleShift(
+                        shift = shift,
+                        modifier = Modifier.fillMaxWidth().padding(bottom = 2.dp)
+                    )
+                }
             }
         }
     }
---
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/mainmenu/more/statistics/StatisticsScreenViewModel.kt
@@ -0,0 +1,70 @@
+package hu.bme.aut.android.securityapp.ui.feature.mainmenu.more.statistics
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import hu.bme.aut.android.securityapp.constants.Constants
+import hu.bme.aut.android.securityapp.constants.LoggedPerson
+import hu.bme.aut.android.securityapp.data.model.shift.Shift
+import hu.bme.aut.android.securityapp.data.repository.ShiftRepository
+import hu.bme.aut.android.securityapp.domain.wrappers.Resource
+import hu.bme.aut.android.securityapp.domain.wrappers.ScreenState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class StatisticsScreenViewModel @Inject constructor(
+    private val shiftRepository: ShiftRepository
+) : ViewModel() {
+
+    private val _screenState = MutableStateFlow<ScreenState>(ScreenState.Loading())
+    val screenState = _screenState.asStateFlow()
+
+
+    private var _allShift = listOf<Shift>()
+
+    private val _shiftsToShow = MutableStateFlow<List<Shift>>(listOf())
+    val shifts = _shiftsToShow.asStateFlow()
+
+    init {
+        loadJudgedShifts()
+    }
+
+    private fun loadJudgedShifts(){
+        viewModelScope.launch(Dispatchers.IO) {
+            _screenState.value = ScreenState.Loading()
+            val shifts = shiftRepository.getAllJudgedShifts(jobId = LoggedPerson.CURRENT_JOB_ID, personId = LoggedPerson.ID)
+
+            when (shifts){
+                is Resource.Success -> {
+                    _screenState.value = ScreenState.Success()
+                    _allShift = shifts.data!!
+                    changeFilter(StatisticsFilter.All)
+                }
+
+                is Resource.Error -> {
+                    _screenState.value = ScreenState.Error(message = shifts.message!!)
+                }
+            }
+        }
+    }
+
+    fun changeFilter(filter: StatisticsFilter){
+        when(filter){
+            StatisticsFilter.All -> _shiftsToShow.value = _allShift.filter { it.status.id != Constants.SHIFT_IN_PROGRESS_STATUS_ID }
+            StatisticsFilter.Accepted -> _shiftsToShow.value = _allShift.filter { it.status.id == Constants.SHIFT_ACCEPTED_STATUS_ID }
+            StatisticsFilter.Denied -> _shiftsToShow.value = _allShift.filter { it.status.id == Constants.SHIFT_DENIED_STATUS_ID }
+            StatisticsFilter.Pending -> _shiftsToShow.value = _allShift.filter { it.status.id == Constants.SHIFT_PENDING_STATUS_ID }
+        }
+    }
+}
+
+sealed class StatisticsFilter(){
+    object All : StatisticsFilter()
+    object Accepted : StatisticsFilter()
+    object Denied : StatisticsFilter()
+    object Pending : StatisticsFilter()
+}
