f4261a9154d908213b08202d37db2dd1702dc33a
Nemes Axel Roland <neaxro@gmail.com>
2023-10-09 23:01:11 +0200

    Create MyTimePicker.kt

---
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/common/MyTimePicker.kt
@@ -0,0 +1,79 @@
+package hu.bme.aut.android.securityapp.ui.feature.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import java.time.LocalDateTime
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTimePicker(
+    date: LocalDateTime,
+    onConfirm: (LocalDateTime) -> Unit,
+    modifier: Modifier = Modifier
+){
+    val timePickerState = remember{
+        TimePickerState(
+            initialHour = date.hour,
+            initialMinute = date.minute,
+            is24Hour = true
+        )
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.background(Color.White)
+    ) {
+        TimePicker(
+            state = timePickerState,
+        )
+
+        OutlinedButton(onClick = {
+            val selectedTime = LocalDateTime.of(
+                date.year,
+                date.monthValue,
+                date.dayOfMonth,
+                timePickerState.hour,
+                timePickerState.minute,
+                date.second,
+                date.nano
+            )
+            onConfirm(selectedTime)
+        }) {
+            Text(text = "Confirm")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MyTimePickerPreview(){
+    var text by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = text)
+
+        MyTimePicker(
+            date = LocalDateTime.now(),
+            onConfirm = {
+                text = it.toString()
+            }
+        )
+    }
+}
