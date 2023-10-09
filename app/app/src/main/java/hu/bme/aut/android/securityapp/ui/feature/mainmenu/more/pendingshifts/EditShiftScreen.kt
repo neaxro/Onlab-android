ae4db4de5a25c96f702a14b545c75f29453c2ea4
Nemes Axel Roland <neaxro@gmail.com>
2023-10-09 23:01:27 +0200

    Implement Time and Date picker dialogs

--- app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/mainmenu/more/pendingshifts/EditShiftScreen.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/mainmenu/more/pendingshifts/EditShiftScreen.kt
@@ -6,23 +6,37 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Wallet
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import hu.bme.aut.android.securityapp.ui.feature.common.DoubleDataRow
+import hu.bme.aut.android.securityapp.ui.feature.common.MyDatePicker
 import hu.bme.aut.android.securityapp.ui.feature.common.MyDropDownMenu
+import hu.bme.aut.android.securityapp.ui.feature.common.MyTimePicker
 import hu.bme.aut.android.securityapp.ui.feature.common.MyTopAppBar
+import java.time.LocalDateTime
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditShiftScreen(
     navigateBack: () -> Unit,
 ){
+    var showDatePicker by remember { mutableStateOf<Boolean>(false) }
+    var titleDatePicker by remember { mutableStateOf<String>("") }
+
+    var showTimePicker by remember { mutableStateOf<Boolean>(false) }
+    var titleTimePicker by remember { mutableStateOf<String>("") }
+
     Scaffold(
         topBar = {
             MyTopAppBar(
@@ -47,7 +61,10 @@ fun EditShiftScreen(
                 DoubleDataRow(
                     title = { Text(text = "Start date:") },
                     value = {
-                        TextButton(onClick = { /*TODO*/ }) {
+                        TextButton(onClick = {
+                            titleDatePicker = "Select Start Date"
+                            showDatePicker = true
+                        }) {
                             Text(text = "2023.04.14")
                         }
                     }
@@ -56,7 +73,10 @@ fun EditShiftScreen(
                 DoubleDataRow(
                     title = { Text(text = "Start time:") },
                     value = {
-                        TextButton(onClick = { /*TODO*/ }) {
+                        TextButton(onClick = {
+                            titleTimePicker = "Select Start Time"
+                            showTimePicker = true
+                        }) {
                             Text(text = "22:50")
                         }
                     }
@@ -65,7 +85,10 @@ fun EditShiftScreen(
                 DoubleDataRow(
                     title = { Text(text = "End date:") },
                     value = {
-                        TextButton(onClick = { /*TODO*/ }) {
+                        TextButton(onClick = {
+                            titleDatePicker = "Select End Date"
+                            showDatePicker = true
+                        }) {
                             Text(text = "2023.04.14")
                         }
                     }
@@ -74,17 +97,11 @@ fun EditShiftScreen(
                 DoubleDataRow(
                     title = { Text(text = "End time:") },
                     value = {
-                        TextButton(onClick = { /*TODO*/ }) {
+                        TextButton(onClick = {
+                            titleTimePicker = "Select End Time"
+                            showTimePicker = true
+                        }) {
                             Text(text = "22:50")
-                        }
-                    }
-                )
-
-                DoubleDataRow(
-                    title = { Text(text = "Start date:") },
-                    value = {
-                        TextButton(onClick = { /*TODO*/ }) {
-                            Text(text = "2023.04.14")
                         }
                     }
                 )
@@ -100,6 +117,35 @@ fun EditShiftScreen(
                         )
                     }
                 )
+            }
+
+            if(showDatePicker) {
+                AlertDialog(
+                    onDismissRequest = { showDatePicker = false },
+                ) {
+                    MyDatePicker(
+                        date = LocalDateTime.now(),
+                        onConfirm = {
+                            showDatePicker = false
+                        },
+                        title = {
+                            Text(text = titleDatePicker)
+                        }
+                    )
+                }
+            }
+
+            if(showTimePicker) {
+                AlertDialog(
+                    onDismissRequest = { showTimePicker = false },
+                ) {
+                    MyTimePicker(
+                        date = LocalDateTime.now(),
+                        onConfirm = {
+                            showTimePicker = false
+                        },
+                    )
+                }
             }
         }
     }
