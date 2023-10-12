336467b1378a7fdba28bafded3dc5afc7e1cf35f
Nemes Axel Roland <neaxro@gmail.com>
2023-10-12 20:12:36 +0200

    Refactor JobDetailScreen and logic

--- app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/mainmenu/jobs/JobDetailViewModel.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/mainmenu/jobs/JobDetailViewModel.kt
@@ -1,36 +1,48 @@
 package hu.bme.aut.android.securityapp.ui.feature.mainmenu.jobs
 
-import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hu.bme.aut.android.securityapp.data.model.job.DetailedJob
-import hu.bme.aut.android.securityapp.data.model.people.Person
 import hu.bme.aut.android.securityapp.data.repository.JobRepository
 import hu.bme.aut.android.securityapp.domain.wrappers.Resource
+import hu.bme.aut.android.securityapp.domain.wrappers.ScreenState
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class JobDetailViewModel @Inject constructor(
-    private val jobRepository: JobRepository
+    private val jobRepository: JobRepository,
+    private val stateHandle: SavedStateHandle,
 ): ViewModel() {
 
-    var job = mutableStateOf(DetailedJob(0, "", "", "", Person(0, "", "", "", "", null)))
+    private val _screenState = MutableStateFlow<ScreenState>(ScreenState.Loading())
+    val screenState = _screenState.asStateFlow()
 
-    fun getJobById(jobId: Int, onError: (String) -> Unit){
+    private val _job = MutableStateFlow<DetailedJob>(DetailedJob())
+    val job = _job.asStateFlow()
+
+    init {
+        val jobId = checkNotNull<Int>(stateHandle["jobId"])
+        getJobById(jobId = jobId)
+    }
+
+    private fun getJobById(jobId: Int){
+        _screenState.value = ScreenState.Loading()
         viewModelScope.launch(Dispatchers.IO) {
             val result = jobRepository.getJobById(jobId)
 
             when(result){
                 is Resource.Success -> {
-                    job.value = result.data!!
+                    _screenState.value = ScreenState.Success()
+                    _job.value = result.data!!
                 }
                 is Resource.Error -> {
-                    viewModelScope.launch(Dispatchers.Main) {
-                        onError(result.message!!)
-                    }
+                    _screenState.value = ScreenState.Error(message = result.message!!)
                 }
             }
         }
