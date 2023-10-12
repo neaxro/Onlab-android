b5a97fb95fa7214f6e08a4a6e827c96ede779786
Nemes Axel Roland <neaxro@gmail.com>
2023-10-12 18:35:33 +0200

    Update MainActivity.kt

--- app/app/src/main/java/hu/bme/aut/android/securityapp/MainActivity.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/MainActivity.kt
@@ -15,40 +15,10 @@ class MainActivity() : ComponentActivity() {
 
         val startDest = Screen.Login.fullRoute
 
-        /*
-        val loginViewModel: LoginViewModel by viewModels<LoginViewModel>()
-        val loginData = getLoginData()
-
-        if(loginData == null){
-            startDest = Screen.Login.fullRoute
-        }
-        else{
-            loginViewModel.username.value = loginData.username
-            loginViewModel.password.value = loginData.password
-
-            loginViewModel.loginUser(
-                onSuccess = {},
-                onError = { startDest = Screen.Login.fullRoute }
-            )
-        }
-        */
-
         setContent {
             SecurityAppTheme {
                 MyAppNavHost(startDestination = startDest)
             }
         }
     }
-
-    /*
-    private fun getLoginData(): LoginData? {
-        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return null
-        val username = sharedPref.getString("USERNAME", null)
-        val password = sharedPref.getString("PASSWORD", null)
-
-        if(username == null || password == null) return null
-
-        return LoginData(username, password)
-    }
-    */
 }
