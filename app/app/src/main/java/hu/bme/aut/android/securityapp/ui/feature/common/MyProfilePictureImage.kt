8fec84b472a08eb2928832a2788a48cbd8ec3353
Nemes Axel Roland <neaxro@gmail.com>
2023-10-19 21:14:28 +0200

    Update MyProfilePictureImage.kt

--- app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/common/MyProfilePictureImage.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/common/MyProfilePictureImage.kt
@@ -3,13 +3,14 @@ package hu.bme.aut.android.securityapp.ui.feature.common
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import hu.bme.aut.android.securityapp.R
 import hu.bme.aut.android.securityapp.data.model.people.Person
 import hu.bme.aut.android.securityapp.data.model.people.toBitmap
 
@@ -19,17 +20,20 @@ fun MyProfilePictureImage(
     modifier: Modifier = Modifier
 ){
     if (profilePicture != null) {
+
         Image(
             bitmap = profilePicture.toBitmap(),
             contentDescription = "Profile picture",
-            modifier = modifier
+            modifier = modifier,
+            contentScale = ContentScale.Crop
         )
     }
     else{
         Image(
-            imageVector = Icons.Rounded.Person,
+            painter = painterResource(id = R.drawable.profile_picture_placeholder),
             contentDescription = "Profile picture",
-            modifier = modifier
+            modifier = modifier,
+            contentScale = ContentScale.Crop
         )
     }
 }
