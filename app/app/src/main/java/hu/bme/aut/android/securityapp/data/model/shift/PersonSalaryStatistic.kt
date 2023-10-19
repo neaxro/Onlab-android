63b95353ab8b370e94a87ad2aafeb422b38ae85c
Nemes Axel Roland <neaxro@gmail.com>
2023-10-19 20:22:31 +0200

    Update PersonSalaryStatistic.kt

--- app/app/src/main/java/hu/bme/aut/android/securityapp/data/model/shift/PersonSalaryStatistic.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/data/model/shift/PersonSalaryStatistic.kt
@@ -3,5 +3,6 @@ package hu.bme.aut.android.securityapp.data.model.shift
 data class PersonSalaryStatistic(
     val personId: Int = 0,
     val personName: String = "",
+    val profilePicture: String? = null,
     val fullSalary: Double = 0.0,
 )
