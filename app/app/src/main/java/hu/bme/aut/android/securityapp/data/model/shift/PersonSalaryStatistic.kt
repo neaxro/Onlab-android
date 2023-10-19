0e9b34c38eb12aa9843d73bba124965eb885c5e5
Nemes Axel Roland <neaxro@gmail.com>
2023-10-19 19:53:42 +0200

    Create PersonSalaryStatistic.kt

---
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/data/model/shift/PersonSalaryStatistic.kt
@@ -0,0 +1,7 @@
+package hu.bme.aut.android.securityapp.data.model.shift
+
+data class PersonSalaryStatistic(
+    val personId: Int = 0,
+    val personName: String = "",
+    val fullSalary: Double = 0.0,
+)
