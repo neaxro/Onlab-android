989a928a1ead2cd55ff1084f1edd13c68cf88f0c
Nemes Axel Roland <neaxro@gmail.com>
2023-10-25 21:48:38 +0200

    Update MyPasswordTextField.kt

--- app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/common/MyPasswordTextField.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/common/MyPasswordTextField.kt
@@ -8,12 +8,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -21,22 +21,23 @@ import androidx.compose.ui.text.input.VisualTransformation
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyPasswordTextField(
-    password: String = "",
+    password: String,
     onPasswordChange: (String) -> Unit = {},
+    label: @Composable() (() -> Unit)?,
     isError: Boolean = false,
     readOnly: Boolean = false,
     enabled: Boolean = true,
+    supportingText: @Composable() (() -> Unit)? = null,
+    modifier: Modifier = Modifier,
 ){
-    var _password by remember { mutableStateOf(password) }
     var passwordVisible by remember { mutableStateOf(false) }
 
     OutlinedTextField(
-        value = _password,
+        value = password,
         onValueChange = {
-            _password = it
             onPasswordChange(it)
         },
-        label = { Text(text = "Password") },
+        label = label,
         singleLine = true,
         trailingIcon = {
             val image = if(passwordVisible){
@@ -60,5 +61,7 @@ fun MyPasswordTextField(
         isError = isError,
         readOnly = readOnly,
         enabled = enabled,
+        supportingText = supportingText,
+        modifier = modifier
     )
 }
