a5b8847cf44adfd47d7a72e0bf24e93621fab2dd
Nemes Axel Roland <neaxro@gmail.com>
2023-10-26 21:16:03 +0200

    Extracted strings from JobCard

--- app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/common/JobCard.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/common/JobCard.kt
@@ -21,11 +21,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import hu.bme.aut.android.securityapp.R
 import hu.bme.aut.android.securityapp.data.model.job.DetailedJob
 import hu.bme.aut.android.securityapp.data.model.people.Person
 import hu.bme.aut.android.securityapp.ui.theme.LimeMain
@@ -81,7 +83,7 @@ fun JobCard(
                     ) {
                         Icon(
                             imageVector = Icons.Rounded.Info,
-                            contentDescription = "Show details",
+                            contentDescription = stringResource(R.string.composable_show_details),
                         )
                     }
                 }
