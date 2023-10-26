60d2d4483140a31443dc52fc9902d3355750ec4d
Nemes Axel Roland <neaxro@gmail.com>
2023-10-26 21:21:28 +0200

    Extracted string from MyDropDownMenu

--- app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/common/MyDropDownMenu.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/common/MyDropDownMenu.kt
@@ -17,8 +17,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import hu.bme.aut.android.securityapp.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -50,7 +52,7 @@ fun MyDropDownMenu(
             leadingIcon = {
                 Icon(
                     imageVector = icon,
-                    contentDescription = "Icon"
+                    contentDescription = stringResource(id = R.string.composable_icon)
                 )
             },
             trailingIcon = {
