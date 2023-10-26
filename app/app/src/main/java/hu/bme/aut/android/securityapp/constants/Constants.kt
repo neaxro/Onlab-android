2c464d3a3a580b40683cca9c12682cf643d87a4d
Nemes Axel Roland <neaxro@gmail.com>
2023-10-26 19:22:13 +0200

    Update Constants.kt

--- app/app/src/main/java/hu/bme/aut/android/securityapp/constants/Constants.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/constants/Constants.kt
@@ -1,9 +1,6 @@
 package hu.bme.aut.android.securityapp.constants
 
 object Constants {
-    const val SERVER_ADDRESS = "http://10.0.2.2:5002/"
-    const val SERVER_ADDRESS_NOT_EMULATOR = "http://192.168.1.69:5002/"
-
     const val SHIFT_PENDING_STATUS_ID = 1
     const val SHIFT_IN_PROGRESS_STATUS_ID = 2
     const val SHIFT_ACCEPTED_STATUS_ID = 3
