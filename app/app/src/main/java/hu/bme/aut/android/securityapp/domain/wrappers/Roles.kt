1fb5a4e69cebc211591a99dc390aed8fbf768f17
Nemes Axel Roland <neaxro@gmail.com>
2023-10-26 20:51:30 +0200

    Extracted strings from Roles

--- app/app/src/main/java/hu/bme/aut/android/securityapp/domain/wrappers/Roles.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/domain/wrappers/Roles.kt
@@ -1,8 +1,15 @@
 package hu.bme.aut.android.securityapp.domain.wrappers
 
-sealed class Roles(val title: String) {
-    class Owner(): Roles("Owner")
-    class Admin(): Roles("Admin")
-    class User(): Roles("User")
-    class Anonymous(): Roles("Anonymous")
+import android.content.Context
+import hu.bme.aut.android.securityapp.MyApp
+import hu.bme.aut.android.securityapp.R
+
+sealed class Roles(
+    val title: String,
+    val context: Context
+) {
+    class Owner(context: Context = MyApp.applicationContext()): Roles(context.getString(R.string.wrappers_roles_owner), context)
+    class Admin(context: Context = MyApp.applicationContext()): Roles(context.getString(R.string.wrappers_roles_admin), context)
+    class User(context: Context = MyApp.applicationContext()): Roles(context.getString(R.string.wrappers_roles_user), context)
+    class Anonymous(context: Context = MyApp.applicationContext()): Roles(context.getString(R.string.wrappers_roles_anonymous), context)
 }
