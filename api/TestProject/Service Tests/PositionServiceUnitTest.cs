d51e7a66d5162094f3a7af75055f53f29f84a880
Nemes Axel Roland <neaxro@gmail.com>
2023-10-29 21:14:40 +0100

    Create Unit test for Position service
    Just a try for a service mock

---
+++ api/TestProject/Service Tests/PositionServiceUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using Moq.EntityFrameworkCore;
+using SecurityApi.Context;
+using SecurityApi.Model;
+using SecurityApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityApiTest.Service_Tests
+{
+    public class PositionServiceUnitTest
+    {
+        Mock<OnlabContext> mock;
+        PositionService positionService;
+
+        public PositionServiceUnitTest()
+        {
+            mock = new Mock<OnlabContext>();
+            positionService = new PositionService(mock.Object);
+        }
+
+        [Fact]
+        public void test()
+        {
+            // Arrange
+            List<Position> positions = DummyDatabase.GetPositions(10);
+            mock.Setup<DbSet<Position>>(c => c.Positions)
+                .ReturnsDbSet(positions);
+
+            // Act
+            var result = positionService.GetAll();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(result.Count(), positions.Count());
+        }
+    }
+}
