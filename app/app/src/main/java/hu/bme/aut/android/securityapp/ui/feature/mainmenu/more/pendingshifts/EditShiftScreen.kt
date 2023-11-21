37f5946dc73eb6d6898653d385c8b159e1b2f196
Nemes Axel Roland <neaxro@gmail.com>
2023-11-21 20:38:50 +0100

    Change rememberSaveable to remember

--- app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/mainmenu/more/pendingshifts/EditShiftScreen.kt
+++ app/app/src/main/java/hu/bme/aut/android/securityapp/ui/feature/mainmenu/more/pendingshifts/EditShiftScreen.kt
@@ -21,7 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -51,7 +51,7 @@ fun EditShiftScreen(
     viewModel: EditShiftViewModel = hiltViewModel()
 ){
     val context = LocalContext.current
-    var whatToShow by rememberSaveable { mutableStateOf<ShowDialog>(ShowDialog.Nothing()) }
+    var whatToShow by remember { mutableStateOf<ShowDialog>(ShowDialog.Nothing()) }
 
     val shift = viewModel.shift.collectAsState().value
     val wages = viewModel.wages.collectAsState().value
